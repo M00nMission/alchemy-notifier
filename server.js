06eb7c9f078ea53a0c28ea5e56299906665013fb
YetiBridge <yetiswap@protonmail.com>
2021-10-24 16:06:25 -0400

    added Alchemy keys

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const path = require('path')
-const socketIO = require('socket.io');
-const PORT = process.env.PORT || 5000
-const fetch = require('node-fetch');
+const express = require("express");
+const path = require("path");
+const socketIO = require("socket.io");
+const PORT = process.env.PORT || 5000;
+const fetch = require("node-fetch");
 
 // start the express server with the appropriate routes for our webhook and web requests
 var app = express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, "public")))
   .use(express.json())
-  .post('/alchemyhook', (req, res) => { notificationReceived(req); res.status(200).end() })
-  .get('/*', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .post("/alchemyhook", (req, res) => {
+    notificationReceived(req);
+    res.status(200).end();
+  })
+  .get("/*", (req, res) => res.sendFile(path.join(__dirname + "/index.html")))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // start the websocket server
 const io = socketIO(app);
 
 // listen for client connections/calls on the WebSocket server
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('register address', (msg) => {
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("disconnect", () => console.log("Client disconnected"));
+  socket.on("register address", (msg) => {
     //send address to Alchemy to add to notification
     addAddress(msg);
   });
@@ -28,24 +31,27 @@ io.on('connection', (socket) => {
 // notification received from Alchemy from the webhook. Let the clients know.
 function notificationReceived(req) {
   console.log("notification received!");
-  io.emit('notification', JSON.stringify(req.body));
+  io.emit("notification", JSON.stringify(req.body));
 }
 
 // add an address to a notification in Alchemy
 async function addAddress(new_address) {
   console.log("adding address " + new_address);
-  const body = { webhook_id: <webhook-id>, addresses_to_add: [new_address], addresses_to_remove: [] };
+  const body = {
+    webhook_id: 151538,
+    addresses_to_add: [new_address],
+    addresses_to_remove: [],
+  };
   try {
-    fetch('https://dashboard.alchemyapi.io/api/update-webhook-addresses', {
-      method: 'PATCH',
+    fetch("https://dashboard.alchemyapi.io/api/update-webhook-addresses", {
+      method: "PATCH",
       body: JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' },
-      headers: { 'X-Alchemy-Token': <Alchemy-auth-key>}
+      headers: { "Content-Type": "application/json" },
+      headers: { "X-Alchemy-Token": imKl0lXoWMyxNsXX4Va - n4aHhvj6B5oJ },
     })
-      .then(res => res.json())
-      .then(json => console.log(json));
-  }
-  catch (err) {
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  } catch (err) {
     console.error(err);
   }
 }
